1828195f0ab5cdef831c3d4035da6b501508a7f7
eugesher <mailbox@eugesher.site>
2020-11-21 22:51:19 +0300

    feat: disabled submit button

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,31 +1,58 @@
-const profileNameError = document.querySelector(`.${popupInputProfileName.id}-error`);
-const profileAboutError = document.querySelector(`.${popupInputProfileAbout.id}-error`);
-const placeNameError = document.querySelector(`.${popupInputPlaceName.id}-error`);
-const placeAboutError = document.querySelector(`.${popupInputPlaceLink.id}-error`);
-
-function showInputError(inputField, errorMessage) {
-  const inputError = inputField.closest('.popup__input-container').querySelector(`.${inputField.id}-error`);
+function showInputError(form, inputField, errorMessage) {
+  const inputError = form.querySelector(`.${inputField.id}-error`);
   inputField.classList.add('popup__input_condition_error');
   inputError.classList.add('popup__input-error_active');
   inputError.textContent = errorMessage;
 }
 
-function hideInputError(inputField) {
-  const inputError = inputField.closest('.popup__input-container').querySelector(`.${inputField.id}-error`);
+function hideInputError(form, inputField) {
+  const inputError = form.querySelector(`.${inputField.id}-error`);
   inputField.classList.remove('popup__input_condition_error');
   inputError.classList.remove('popup__input-error_active');
   inputError.textContent = emptyString;
 }
 
-function isValid(inputField) {
+function isValid(form, inputField) {
   if (!inputField.validity.valid) {
-    showInputError(inputField, inputField.validationMessage);
+    showInputError(form, inputField, inputField.validationMessage);
   } else {
-    hideInputError(inputField);
+    hideInputError(form, inputField);
   }
 }
 
-popupInputProfileName.addEventListener('input', () => isValid(popupInputProfileName));
-popupInputProfileAbout.addEventListener('input', () => isValid(popupInputProfileAbout));
-popupInputPlaceName.addEventListener('input', () => isValid(popupInputPlaceName));
-popupInputPlaceLink.addEventListener('input', () => isValid(popupInputPlaceLink));
+function setEventListeners(form){
+  const inputList = Array.from(form.querySelectorAll('.popup__input'));
+  const submitButton = form.querySelector('.popup__submit-button');
+  console.log(submitButton);
+
+  toggleButtonState(inputList, submitButton);
+
+  inputList.forEach(inputField => {
+    inputField.addEventListener('input', () => {
+      isValid(form, inputField);
+      toggleButtonState(inputList, submitButton);
+    });
+  });
+}
+
+function enableValidation() {
+  Array.from(document.querySelectorAll('.popup__container')).forEach((form) => {
+    setEventListeners(form);
+  });
+}
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputField) => {
+    return !inputField.validity.valid;
+  });
+}
+
+function toggleButtonState (inputList, submitButton) {
+  if (hasInvalidInput(inputList)) {
+    submitButton.setAttribute('disabled', '');
+  } else {
+    submitButton.removeAttribute('disabled');
+  }
+}
+
+enableValidation();
