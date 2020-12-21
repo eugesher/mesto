1543c332f346f123279f93723ff40d84af9176f7
eugesher <mailbox@eugesher.site>
2020-12-21 20:41:16 +0300

    refactor: declare Popup._handleEscClose method

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,6 +3,7 @@ export class Popup {
     this._popupSelector = popupSelector;
     this._popup = document.querySelector(this._popupSelector);
     this._closeButton = this._popup.querySelector(".popup__close-button");
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   _handleEscClose(evt) {
@@ -17,12 +18,12 @@ export class Popup {
   }
 
   open() {
-    document.addEventListener("keydown", this._handleEscClose.bind(this));
+    document.addEventListener("keydown", this._handleEscClose);
     this._popup.classList.add("popup_opened");
   }
 
   close() {
-    document.removeEventListener("keydown", this._handleEscClose.bind(this));
+    document.removeEventListener("keydown", this._handleEscClose);
     this._popup.classList.remove("popup_opened");
   }
 }
