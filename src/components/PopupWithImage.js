9f676be777ea0e408739b217d394af18e9e6a28d
Evgenii Chernyshov <mailbox@eugesher.site>
2021-01-19 22:24:27 +0300

    refactor(PopupWithImage.js): rename fields

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,10 +3,10 @@ import { Popup } from "./Popup.js";
 export class PopupWithImage extends Popup {
   open(imageName, imageLink) {
     super.open();
-    this._popupImage = this._popup.querySelector(".popup__image");
-    this._popupImageCaption = this._popup.querySelector(".popup__image-caption");
-    this._popupImage.setAttribute("src", imageLink);
-    this._popupImage.setAttribute("alt", imageName);
-    this._popupImageCaption.textContent = imageName;
+    this._image = this._popup.querySelector(".popup__image");
+    this._imageCaption = this._popup.querySelector(".popup__image-caption");
+    this._image.setAttribute("src", imageLink);
+    this._image.setAttribute("alt", imageName);
+    this._imageCaption.textContent = imageName;
   }
 }
